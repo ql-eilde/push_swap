fa3b4fd59b9c2f837fa0e1a9a659dd6f9064e4c0
Quentin L-EILDE <ql-eilde@e2r7p23.42.fr>
2015-01-27 20:41:47 +0100

    coucou

--- pushswap.c
+++ pushswap.c
@@ -6,7 +6,7 @@
 /*   By: ql-eilde <ql-eilde@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/26 19:25:16 by ql-eilde          #+#    #+#             */
-/*   Updated: 2015/01/26 20:06:08 by ql-eilde         ###   ########.fr       */
+/*   Updated: 2015/01/27 20:38:23 by ql-eilde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@ int		*ft_char_to_int(int argc, char **argv)
 
 	i = 0;
 	j = 1;
-	a = (int *)malloc(sizeof(a) * argc);
+	a = (int *)malloc(sizeof(a) * (argc - 1));
 	while (argv[j] != NULL)
 	{
 		a[i] = ft_atoi(argv[j]);
-		ft_putnbr(a[i]), ft_putchar('\n');
 		j++;
 		i++;
 	}
@@ -36,9 +35,66 @@ void	ft_error(char **str)
 	return ;
 }
 
-void	ft_push_swap(int *tab)
+int		ft_isgrowing(int *tab, int argc)
 {
+	int		i;
 
+	i = 0;
+	while (i < (argc - 1))
+	{
+		if (tab[i] > tab[i + 1])
+			i++;
+		else
+			break ;
+	}
+	if (i == (argc - 2))
+		return (1);
+	return (0);
+}
+
+void	ft_push_swap(int *a, int argc)
+{
+	int		*b;
+	int		index;
+	int		save;
+	int		i;
+
+	b = (int *)malloc(sizeof(b) * (argc - 1));
+	while (ft_isgrowing(a, argc) != 1 || ft_isgrowing(b, argc) != 1)
+	{
+		index = (argc - 2);
+		i = 0;
+		while (index >= 0)
+		{
+			if (a[index] > a[index - 1])
+			{
+				save = a[index];
+				a[index] = a[index - 1];
+				a[index - 1] = save, ft_putstr("sa ");
+			}
+			else if (a[index] < a[index - 1])
+			{
+				b[i] = a[index];
+				i++, index--, ft_putstr("pb ");
+			}
+		}
+		index = (argc - 2);
+		i = 0;
+		while (index >= 0)
+		{
+			if (b[index] > b[index - 1])
+			{
+				a[i] = b[index];
+				i++, index--, ft_putstr("pa ");
+			}
+			else if (b[index] < b[index - 1])
+			{
+				save = b[index];
+				b[index] = b[index - 1];
+				b[index - 1] = save, ft_putstr("sb ");
+			}
+		}
+	}
 }
 
 int		main(int argc, char **argv)
@@ -47,6 +103,7 @@ int		main(int argc, char **argv)
 
 //	ft_error(argv);
 	a = ft_char_to_int(argc, argv);
-//	ft_push_swap(a);
+	ft_putnbr(ft_isgrowing(a, argc));
+//	ft_push_swap(a, argc);
 	return (0);
 }
--- pushswap.h
+++ pushswap.h
@@ -6,7 +6,7 @@
 /*   By: ql-eilde <ql-eilde@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/26 19:42:59 by ql-eilde          #+#    #+#             */
-/*   Updated: 2015/01/26 20:00:58 by ql-eilde         ###   ########.fr       */
+/*   Updated: 2015/01/27 17:27:12 by ql-eilde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int		*ft_char_to_int(int argc, char **argv);
 void	ft_error(char **str);
-void	ft_push_swap(int *tab);
+void	ft_push_swap(int *a, int argc);
+int		ft_isgrowing(int *tab, int argc);
 
 #endif
